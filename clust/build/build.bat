c13590115e8139de528d206ea0e6bc09c37067a6
Dianeod <dodonoghue@kx.com>
2020-04-23 09:41:38 +0100

    added echo$

--- clust/build/build.bat
+++ clust/build/build.bat
@@ -7,8 +7,8 @@ curl -fsSL -o ../src/k.h   https://github.com/KxSystems/kdb/raw/master/c/c/k.h  
 
 ::keep original PATH, PATH may get too long otherwise
 set OP=%PATH%
-set VS =$(ls C:\Program Files (x86)\Microsoft Visual Studio)
-echo "ls $VS"
+VS =$(ls C:\Program Files (x86)\Microsoft Visual Studio)
+echo $VS
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 cl /LD /DKXVER=3 ../src/kdnn.c q.lib
 set PATH=%OP%
