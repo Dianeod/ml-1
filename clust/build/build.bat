483c4207cedd0c8667d170767e10218b42b83cd0
Dianeod <dodonoghue@kx.com>
2020-05-04 20:34:07 +0100

    added if statement

--- clust/build/build.bat
+++ clust/build/build.bat
@@ -8,11 +8,22 @@ curl -fsSL -o ../src/k.h   https://github.com/KxSystems/kdb/raw/master/c/c/k.h  
 ::keep original PATH, PATH may get too long otherwise
 set OP=%PATH%
 
-if EXIST "\Program Files (x86)\Microsoft Visual Studio\2018\Community\VC\Auxiliary\Build\vcvars64.bat" (
-	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
-) else (	
-  call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Buildtools\VC\Auxiliary\Build\vcvars64.bat"
+VS = args[0]
+
+if %VS%==[] ECHO VISUAL STUDIO year missing
+
+if %VS%==2017 (%DIR% = "Community"
+) elif %VS%==2019 (%DIR% = "Buildtools"
 )
+
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\%VS%\%DIR%\Auxiliary\Build\vcvars64.bat"
+
+:: if EXIST "\Program Files (x86)\Microsoft Visual Studio\2018\Community\VC\Auxiliary\Build\vcvars64.bat" (
+::	call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+::) else (	
+ :: call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Buildtools\VC\Auxiliary\Build\vcvars64.bat"
+::)
 
 cl /LD /DKXVER=3 ../src/kdnn.c q.lib
 set PATH=%OP%
