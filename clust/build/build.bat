308130575ff7ece38136d9410fc5d73adeb3c1bf
Dianeod <dodonoghue@kx.com>
2020-05-06 12:21:11 +0100

    added in vsyear

--- clust/build/build.bat
+++ clust/build/build.bat
@@ -18,6 +18,11 @@ IF NOT "%VSDIR%" == "" (
       goto :VS%year%
 )      
 
+:VS2017
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+:VS2019
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Buildtools\VC\Auxiliary\Build\vcvars64.bat"
 
 cl /LD /DKXVER=3 ../src/kdnn.c q.lib
 set PATH=%OP%
